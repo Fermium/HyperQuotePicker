2594c3c258201cff510ba3805343bf9b8eb7e5ec
Simone Tosato <s@fermiumlabs.com>
2017-11-24 11:31:47 +0100

    improved code

--- quotePicker.R
+++ quotePicker.R
@@ -1,67 +1,55 @@
-#install.packages("doMC")
-#install.packages("gtools")
-#install.packages("tictoc")
-#install.packages("ggplot2")
-#install.packages("gridExtra")
-#install.packages("gplots")
-#install.packages("data.table")
-
+#Install missing requirements and load them
+rm(list = ls())
+reqpackages <- c("doMC","gtools","tictoc","ggplot2","gridExtra","gplots","data.table","foreach","compiler","future","optparse","purrr","tibble")
+new.packages <- reqpackages[!(reqpackages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(reqpackages, require, character.only = TRUE)
 
 # clean up varialble
 rm(list = ls())
+option_list = list(
+  make_option(c("-f", "--file"), type="character", default="example.csv", 
+              help="csv format quoteDataset file name [default=%default]", metavar="character"),
+  make_option(c("-o", "--out"), type="character", default="best", 
+              help="output files name [default= %default]", metavar="character"),
+  make_option(c("-n","--maxCheap"),type="integer",default = 3,
+              help="number of output for each combination [default = %default",metavar="integer")
+  )
 
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
 #### CONFIG
-CPUCores <- 32
-MaxCheapest <- 3 # Max number of cheapest lines for each amount of suppliers to shop display
-inputFileName <- "example.csv"
-outputFileName <- "best.csv"
-outputGraphsFileName <- "bests.pdf"
-
-
-# Libraries
-library(foreach)
-library(doMC)
-registerDoMC(CPUCores)
-library(gtools)
-library(tictoc)
-require(ggplot2)
-library(gridExtra)
-library(gplots)
-require(compiler)
-require(data.table)
+CPUCores <- availableCores()
+MaxCheapest <- opt$maxCheap # Max number of cheapest lines for each amount of suppliers to shop display
+inputFileName <- opt$file
+outputFileName <- paste(opt$out,"csv",sep=".")
+outputGraphsFileName <- paste(opt$out,"pdf",sep=".")
 
 enableJIT(3)
 tic()
-
-
-
-pdf("bests.pdf", paper="a4")
+pdf(outputGraphsFileName, paper="a4")
 
 sprintf("The script will be run with %d CPU cores, be sure this matches the number of cores on your system", CPUCores)
 
-data <- read.csv(inputFileName)
+quoteData <- read.csv(inputFileName)
 
 #Extract suppliers and pieces list
-suppliers <- unique(data$Supplier)
-pieces <- unique(data$Piece)
+suppliers <- unique(quoteData$Supplier)
+pieces <- unique(quoteData$Piece)
 
 grid.table((suppliers))
 grid.table(pieces)
 
 
 #Only take the first five colums
-data <- data[, 1:5]
+quoteData <- quoteData[, 1:5]
 
 # Remove commas and dollar sign from the prices
-data$Price <- as.numeric(gsub('[$,€]', '', data$Price))
-data$Tot <- as.numeric(gsub('[$,€]', '', data$Tot))
+quoteData$Price <- as.numeric(gsub('[$,€]', '', quoteData$Price))
+quoteData$Tot <- as.numeric(gsub('[$,€]', '', quoteData$Tot))
 
 # The piece list contains every piece possible suppliers and prices
-piece <- list()
-for (pp in unique(data$Piece)) {
-  piece[[pp]] <- data[data$Piece == pp, 2:5]
-  piece[[pp]] <- piece[[pp]][order(piece[[pp]]$Supplier), ]
-}
+pieceList <-split(quoteData,quoteData$Piece)
 
 #Generate all possible combinations of pieces and suppliers
 #These are only indexes, and will need to be threated accordingl
@@ -83,24 +71,11 @@ sprintf(
 )
 
 # Calculate the total price for each combination
-totals <- as.data.frame(matrix(NA, dim(x)[1], length(suppliers) + 2))
-totals <- foreach (j=1:dim(x)[1], .combine=function(...) rbindlist(list(...)), .multicombine = TRUE) %dopar% {
-  tot <- 0
-  supps <- c()
-  i <- 1
-  rrow <- c()
-  for (pp in pieces) {
-    tot <- tot + piece[[pp]]$Tot[x[j, i]]
-    supps <- c(supps, piece[[pp]]$Supplier[x[j, i]])
-    rrow <- cbind(rrow, as.numeric(piece[[pp]]$Supplier[x[j, i]]))
-    i <- i + 1
-  }
-  cbind(tot, length(unique(supps)), as.data.frame(rrow))
-}
-# convert back to data.frame
-totals <- as.data.frame(totals)
-
-
+totals <- foreach (j=1:dim(x)[1], .combine=function(...) rbind(...), .multicombine = TRUE) %dopar% {
+  tot <- sum(mapply(function(pp,index,i){pp$Tot[index[i]]},pieceList,x[j,],SIMPLIFY = T))
+  supps <- mapply(function(pp,index,i){pp$Supplier[index[i]]},pieceList,x[j,],SIMPLIFY =T)
+  c(tot, length(unique(supps)),supps)
+} %>% as_tibble(.)
 
 #Rename columns
 colnames(totals) <- c('price', 'nOfSuppliers', levels(pieces))
@@ -119,16 +94,16 @@ for(i in 1:MaxCheapest){
 best <- as.data.frame(matrix(NA, 0, length(suppliers) + 2))
 best <- foreach (i= 1:min(length(suppliers),length(pieces)), .combine=rbind) %dopar%{
   tmp <- totals[totals$nOfSuppliers == i, ]
-  tmp <- tmp[order(tmp$price), ]
+  tmp<-tmp[order(tmp$price), ]  
   tmp[1:MaxCheapest, ]
 }
 
 # Replace suppliers "number" with the name string
 for (i in 3:(length(pieces) + 2)) {
-  best[, i] <- levels(suppliers)[best[, i]]
+  best[, i] <- levels(suppliers)[best[[i]]]
 }
 
 
-write.csv(best, file = "best.csv")
+write.csv(best, file = outputFileName)
 dev.off()
 toc()
